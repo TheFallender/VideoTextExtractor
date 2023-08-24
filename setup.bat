2f100c65bea6d4f8d28b4a9158421bf7641325ff
TheFallender <a.ferrari@thefallen.io>
2023-08-24 16:17:04 +0200

    Setup and Requirements

---
+++ setup.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+@REM install pyenv through pip
+python -m pip install virtualenv
+
+@REM create a virtual environment
+python -m virtualenv env
+
+@REM activate the virtual environment
+call env\Scripts\activate.bat
+
+@REM install the required packages
+python -m pip install -r requirements.txt
