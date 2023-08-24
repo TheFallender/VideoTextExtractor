d94d38f65b62953f0c7eeca532d3a2a72b1977bd
TheFallender <a.ferrari@thefallen.io>
2023-08-24 16:57:49 +0200

    VideoOCR - Fixed saving frames and data

--- videoOCR.py
+++ videoOCR.py
@@ -18,6 +18,7 @@ import argparse
 # Get cv2 cuda if available
 try:
     import cv2.cuda
+
     cudaDevices = cv2.cuda.getCudaEnabledDeviceCount()
     if cudaDevices == 0:
         # set normal cv2
@@ -39,36 +40,58 @@ from tqdm import tqdm
 # Threading
 from concurrent.futures import ThreadPoolExecutor
 
+
+# Class for the options for the program
+class VideoOCROptions:
+    def __init__(self, video_uri, regex, output, skip_frames, resolution, no_image):
+        self.video_uri = video_uri
+        self.regex = regex
+        self.output = output
+        self.skip_frames = skip_frames
+        self.resolution = resolution
+        self.no_image = no_image
+
+
 # Argument parser
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Extracts frames from a video and performs OCR to search for a regex match.")
-    parser.add_argument('-v', '--video', required=True, help="Path to the video file.")
-    parser.add_argument('-r', '--regex', required=True, help="Regex pattern to search for.")
-    parser.add_argument('-o', '--output', required=True, help="Output folder to save matched frames.")
-    parser.add_argument('-s', '--skip', default=0, help="Number of frames to skip between each frame to process.")
-    parser.add_argument('-q', '--quality', required=False, default="1080p60", help="Resolution of the video to download from YouTube. Default is 1080.")
-    
+    parser = argparse.ArgumentParser(
+        description="Extracts frames from a video and performs OCR to search for a regex match.")
+    parser.add_argument('-v', '--video', required=True,
+                        help="Path to the video file.")
+    parser.add_argument('-r', '--regex', required=True,
+                        help="Regex pattern to search for.")
+    parser.add_argument('-o', '--output', required=True,
+                        help="Output folder to save matched frames.")
+    parser.add_argument('-s', '--skip', default=0,
+                        help="Number of frames to skip between each frame to process.")
+    parser.add_argument('-q', '--quality', required=False, default="1080p60",
+                        help="Resolution of the video to download from YouTube. Default is 1080.")
+    parser.add_argument('--no-image', action='store_true', required=False, default=False,
+                        help="Do not save the matched frames as images.")
+
     return parser.parse_args()
 
-def process_frame(frame, regex, output, timestamp):
+
+def process_frame(frame, timestamp, options: VideoOCROptions):
     # Perform OCR on the frame
     text = ocr_frame(frame)
 
     # Match the extracted text to the regex pattern
-    matches = match_regex(text, regex)
+    matches = match_regex(text, options.regex)
 
     # If a match is found, save the frame and timestamp
     if matches:
-        save_results(frame, matches, output, timestamp)
+        save_results(frame, matches, timestamp, options)
+
 
 # Process the video file
-def process_video(video_uri, regex, output, skip_frames, resolution):
-    if ("youtube.com" in video_uri or "youtu.be" in video_uri):
+def process_video(options: VideoOCROptions):
+    if "youtube.com" in options.video_uri or "youtu.be" in options.video_uri:
         # Get the video capture
-        cap = cap_from_youtube(video_uri, resolution=resolution)
+        cap = cap_from_youtube(options.video_uri, resolution=options.resolution)
     else:
         # Open the video file
-        cap = cv2.VideoCapture(video_uri) 
+        cap = cv2.VideoCapture(options.video_uri)
 
     # Check if video opened successfully
     if not cap.isOpened():
@@ -86,19 +109,25 @@ def process_video(video_uri, regex, output, skip_frames, resolution):
                 break
 
             # Skip frames if necessary
-            if i % skip_frames != 0:
+            if i % options.skip_frames != 0:
                 continue
 
             # Current frame timestamp
-            timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
-            timestamp = time.strftime("%H:%M:%S", time.gmtime(timestamp/1000.0))
-            
-            # Process the frame
-            executor.submit(process_frame, frame, regex, output, timestamp)
+            timestamp_ms = cap.get(cv2.CAP_PROP_POS_MSEC)
 
+            # Extract the whole seconds and the remaining milliseconds
+            seconds = int(timestamp_ms // 1000)
+            milliseconds = int(timestamp_ms % 1000)
+
+            # Format the seconds into HH:MM:SS and append the milliseconds
+            timestamp = time.strftime("%H:%M:%S", time.gmtime(seconds)) + f".{milliseconds:03}"
+
+            # Process the frame
+            executor.submit(process_frame, frame, timestamp, options)
 
     # Release the video capture object
     cap.release()
+
 
 # OCR a single frame
 def ocr_frame(frame):
@@ -110,31 +139,44 @@ def ocr_frame(frame):
 
     return text
 
+
 # Match the extracted text to a regex pattern
 def match_regex(text, regex_pattern):
     # Search for the regex pattern in the extracted text
     matches = re.findall(regex_pattern, text)
-    
+
     return matches
 
+
 # Save the results
-def save_results(frame, matches, output, timestamp):
+def save_results(frame, matches, timestamp, options: VideoOCROptions):
     # Create the output folder if it doesn't exist
-    os.makedirs(output, exist_ok=True)
-    
+    os.makedirs(options.output, exist_ok=True)
+
     # Save the frame as an image
-    image_path = os.path.join(output, f"frame_{timestamp}.png")
+    image_path = os.path.join(options.output, f"frame_{timestamp.replace(':','')}.png")
     cv2.imwrite(image_path, frame)
 
     # Log the timestamp and matches to a text file
-    log_path = os.path.join(output, "matches.txt")
+    log_path = os.path.join(options.output, "matches.txt")
     with open(log_path, 'a') as log_file:
         log_file.write(f"Timestamp: {timestamp}\nMatches: {matches}\n\n")
+
 
 # Main function
 if __name__ == "__main__":
     # Parse the arguments
     args = parse_arguments()
 
+    # Create the options object
+    video_ocr_options = VideoOCROptions(
+        args.video,
+        args.regex,
+        args.output,
+        int(args.skip),
+        args.quality,
+        args.no_image
+    )
+
     # Process the video file
-    process_video(args.video, args.regex, args.output, int(args.skip), args.quality)
+    process_video(video_ocr_options)
